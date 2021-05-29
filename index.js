be348cc4acfbdc8e2f43a17c2747c698bff13fbc
Osmani <osmanihernandezperez@gmail.com>
2021-05-29 16:29:02 -0400

    setting up for deployment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("response-body", (req, res) => {
@@ -112,7 +114,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
